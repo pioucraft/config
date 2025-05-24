dc538e18023aee2905aaecc85349d66f2e715c87
Nathan Gasser <hello@gougoule.ch>
2025-05-24 10:01:08 +0200

    Add git backup command

---
+++ bash/scripts/git.sh
@@ -0,0 +1,3 @@
+gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/repos?per_page=100 | jq -r '.[].full_name' | while read -r repo_full_name; do   owner=$(echo "$repo_full_name" | cut -d/ -f1);   repo_name=$(echo "$repo_full_name" | cut -d/ -f2)
+  mkdir -p "$owner"
+  git clone --mirror "https://github.com/$repo_full_name.git" "$owner/$repo_name.git"; done
