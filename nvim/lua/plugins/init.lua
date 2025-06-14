1038e5b0543edbcbed2fa8dc96d89647a3d17567
Nathan Gasser <hello@gougoule.ch>
2025-06-14 20:49:13 +0200

    Add telescope to nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -60,7 +60,18 @@ return {
     })
     vim.cmd.colorscheme("catppuccin")
   end,
-}
+},
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.git_files, { desc = 'Telescope find files' })
+            vim.keymap.set("n", "<leader>pg", builtin.live_grep, {desc = "Grep search"})
+        end,
+    }
 
 }
 
